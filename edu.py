09f89b78feeaf5bba497c460ef00577f3479f548
Unitmaps <lagirmaks@gmail.com>
2025-12-06 19:12:06 +0200

    Delete edu.py

--- edu.py
+++
@@ -1,6 +0,0 @@
-import datetime as dt
-def log_attempt(attempt, value_at_index, hint):
-    with open("search_log.csv", "a") as f:
-        f.write(f",{dt.datetime.now()},attempt:{attempt},value:{value_at_index},result:{hint}")
-
-log_attempt(1, 28, "greater_then_choice")
